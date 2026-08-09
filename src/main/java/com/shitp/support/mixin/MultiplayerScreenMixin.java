2c8db32f9b483b279f96e59681aebf74af86897b
Creaddinscart <mail@shit.pub>
2026-08-09 11:55:37 +0800

    Update MultiplayerScreenMixin.java

--- src/main/java/com/shitp/support/mixin/MultiplayerScreenMixin.java
+++ src/main/java/com/shitp/support/mixin/MultiplayerScreenMixin.java
@@ -2,6 +2,7 @@ package com.shitp.support.mixin;
 
 import com.shitp.support.screen.ResourceManagerScreen;
 import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screen.TitleScreen;
 import net.minecraft.client.gui.screen.multiplayer.MultiplayerScreen;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.text.Text;
@@ -10,22 +11,29 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(MultiplayerScreen.class)
-public class MultiplayerScreenMixin extends Screen {
+@Mixin({MultiplayerScreen.class, TitleScreen.class})
+public class ScreenMixin extends Screen {
 
-    protected MultiplayerScreenMixin(Text title) {
+    protected ScreenMixin(Text title) {
         super(title);
     }
 
     @Inject(method = "init", at = @At("RETURN"))
     private void addShitpButton(CallbackInfo ci) {
+        int posX = 5;
+        int posY = 5;
+
+        if ((Object) this instanceof TitleScreen) {
+            posY = 25; // 避免挡住主菜单原版按钮
+        }
+
         this.addDrawableChild(
             ButtonWidget.builder(Text.literal("Shitp Manager"), button -> {
                 if (this.client != null) {
                     this.client.setScreen(new ResourceManagerScreen(this));
                 }
             })
-            .dimensions(5, 5, 120, 20)
+            .dimensions(posX, posY, 120, 20)
             .build()
         );
     }
